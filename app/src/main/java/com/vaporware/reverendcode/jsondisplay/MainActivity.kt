61fa75ea096e40b786374a8a8abb423a975cc3c1
David Wenzel <codenull@gmail.com>
2017-07-24 00:44:56 -0700

    Initial commit

---
+++ app/src/main/java/com/vaporware/reverendcode/jsondisplay/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.vaporware.reverendcode.jsondisplay
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.button
+import org.jetbrains.anko.sdk25.coroutines.onClick
+import org.jetbrains.anko.textView
+import org.jetbrains.anko.verticalLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        displayUI()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun displayUI() {
+        verticalLayout {
+            val mTextView = textView {
+                hint = "Json will appear here"
+            }
+            button {
+                text = "Click me for JSON"
+                onClick { //This supports automatic coroutines (WARNING: EXPERIMENTAL)
+                    mTextView.text = "Call your JSON here."
+                }
+            }
+
+        }
+    }
+}
